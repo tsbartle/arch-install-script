85625fde5a87f39c9a309353feb1e65832a9ab95
Travis Bartlett <tsbartle@gmail.com>
2019-11-04 13:45:30 -0500

    wip

--- script/base.sh
+++ script/base.sh
@@ -223,7 +223,7 @@ function update_mirrors() {
 # ARGS: None
 # OUTS: None
 function bootstrap_arch() {
-    pacstrap /mnt base base-devel linux linux-firmware git sudo efibootmgr wpa_supplicant dialog intel-ucode neovim lzop lvm2
+    pacstrap /mnt base base-devel linux linux-firmware sudo efibootmgr wpa_supplicant dialog intel-ucode lzop lvm2 sshd dhcpcd
     genfstab -U -p /mnt > /mnt/etc/fstab
 
     # Copy chroot.sh to chrooted environment
